0a2392cf00b54729898083371b9d029b883335c5
Pramotjonsungnoen <92082350+Pramotjonsungnoen@users.noreply.github.com>
2022-06-20 09:53:53 +0700

    Update blocks.js

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -1,37 +1,12 @@
 Blockly.defineBlocksWithJsonArray([
 {
   "type": "sonar_read",
-  "message0": "sonar pin: %1 echo pin: %2 read distance (cm)",
+  "message0": "sonar pin: %1 read distance (cm)",
   "args0": [
     {
-      "type": "field_dropdown",
+      "type": "input_value",
       "name": "trig",
-      "options": [
-        [
-          "OUT1",
-          "26"
-        ],
-        [
-          "OUT2",
-          "27"
-        ],
-        [
-          "23",
-          "23"
-        ],
-        [
-          "19",
-          "19"
-        ],
-        [
-          "18",
-          "18"
-        ],
-        [
-          "VN",
-          "39"
-        ]
-      ]
+      
     }
   ],
   "output": "Number",
