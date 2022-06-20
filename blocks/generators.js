896ff237336bd6fd96bd13585d3ee1c67ff754e5
Pramotjonsungnoen <92082350+Pramotjonsungnoen@users.noreply.github.com>
2022-06-20 16:49:56 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,9 +1,9 @@
 Blockly.Python['sonar_read'] = function(block) {
-Blockly.Python.definitions_['import_ultrasonic'] = 'import Ultrasonic';
+Blockly.Python.definitions_['import_sonar'] = 'import sonar';
 
   //var dropdown_trig = block.getFieldValue('trig');
   var value_seaLevelhPa = Blockly.Python.valueToCode(block, 'trig', Blockly.Python.ORDER_ATOMIC);
   //var code = `Ultrasonic.read(32)`;
-  var code = `Ultrasonic.read(${value_seaLevelhPa})`;
+  var code = `sonar.read(${value_seaLevelhPa})`;
   return [code, Blockly.Python.ORDER_NONE];
 };
