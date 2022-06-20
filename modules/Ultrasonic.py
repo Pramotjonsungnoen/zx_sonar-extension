29d0e1baf2e6d44ef9c8a8dbc76cbcef3a787d57
Pramotjonsungnoen <92082350+Pramotjonsungnoen@users.noreply.github.com>
2022-06-20 10:39:35 +0700

    Update Ultrasonic.py

--- modules/Ultrasonic.py
+++ modules/Ultrasonic.py
@@ -1,20 +1,12 @@
 from machine import Pin
-from machine import time_pulse_us
-from time import sleep_us
+from machine import ADC
 
-def read(trig_pin, echo_pin):
-    trigger = Pin(trig_pin, mode=Pin.OUT)
-    echo = Pin(echo_pin, mode=Pin.IN)
-
-    trigger.value(0)
-    sleep_us(5)
-    trigger.value(1)
-    sleep_us(10)
-    trigger.value(0)
-
+def read(analog_pin):
+    adc = ADC(Pin(analog_pin))
+    adc.atten(ADC.ATTN_11DB)
+    adc.width(ADC.WIDTH_12BIT)
     try:
-        pulse_time = time_pulse_us(echo, 1, 1000000)
-        d = (pulse_time / 2) / 29.1
-        return int(d) if d < 400 else -1
+        d = adc.read()
+        return int(d) 
     except OSError as ex:
         return -1
